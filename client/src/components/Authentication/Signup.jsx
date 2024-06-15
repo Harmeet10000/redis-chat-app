f7f744b60f2ee4afa663cd2687ba2c9b11178105
Harmeet10000 <harmeetsinghfbd@gmail.com>
2024-06-15 20:31:56 +0530

    added chatContext

--- client/src/components/Authentication/Signup.jsx
+++ client/src/components/Authentication/Signup.jsx
@@ -100,9 +100,9 @@ const Signup = () => {
     if (pics.type === "image/jpeg" || pics.type === "image/png") {
       const data = new FormData();
       data.append("file", pics);
-      data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+      data.append("upload_preset", "MERN-CHAT-APP");
+      data.append("cloud_name", "dkiowo64c");
+      fetch("https://api.cloudinary.com/v1_1/dkiowo64c/upload", {
         method: "post",
         body: data,
       })
